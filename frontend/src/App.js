1c8a912147c5e1b726b52fac9067e9fa9cfd0183
rdevcharles <rondell.charles@generalassemb.ly>
2022-11-22 13:23:52 -0500

    code clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,29 +3,27 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/ui/NavBar";
 import UserAuth from "./pages/UserAuth";
-import AdminDashboard from "./pages/AdminDashboard";
 import Dashboard from "./pages/Dashboard";
 import Games from "./pages/Games";
 import GameDetail from "./pages/GameDetail";
 import ProfileSettings from "./pages/ProfileSettings";
 import { getUser } from "./utils/users-service";
-import Issues from "./pages/Issues";
+
 
 function App() {
   const [user, setUser] = useState(getUser());
 
   return (
     <div className="App">
-     
+     <NavBar/>
       <Routes>
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/admin/issues" element={<Issues/>} />
-        {/* <Route
-          path="/"
+
+        <Route
+          path="/home"
           element={
             user ? <Dashboard user={user} /> : <UserAuth setUser={setUser} />
           }
-        /> */}
+        />
         <Route path="/" element={<Games user={user} />} />
         <Route
           path="/profile-settings"
